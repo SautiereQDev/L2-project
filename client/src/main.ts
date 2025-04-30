25a04958579e3528e74b1d570af0829195fe4d02
SautiereQDev <contact@quentinsautiere.com>
2025-05-01 00:00:05 +0200

    feat: update environment configuration and add axios and vue-query dependencies

--- client/src/main.ts
+++ client/src/main.ts
@@ -3,10 +3,12 @@ import { createPinia } from "pinia";
 
 import App from "./App.vue";
 import router from "./router";
+import vueQueryPlugin from "./plugins/vue-query";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(router);
+app.use(vueQueryPlugin);
 
 app.mount("#app");
--- client/vite.config.ts
+++ client/vite.config.ts
@@ -7,12 +7,23 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(), vueDevTools(), tailwindcss(), AutoImport()],
+  plugins: [vue(), vueDevTools(), tailwindcss()],
   resolve: {
     alias: {
       "@": fileURLToPath(new URL("./src", import.meta.url)),
     },
   },
+  server: {
+    // Proxy pour éviter les problèmes de CORS et de certificats
+    proxy: {
+      '/api': {
+        target: 'https://project.localhost:8443',
+        changeOrigin: true,
+        secure: false, // Désactive la vérification du certificat SSL
+        rewrite: (path) => path.replace(/^\/api/, '/api/v1')
+      }
+    }
+  }
 });
 function AutoImport(): import("vite").PluginOption {
   throw new Error("Function not implemented.");
